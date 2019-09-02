fecbb66f4c0c55f06ebbae4ed202310d88f828aa
Ramanangee <ramananmaya2000@gmail.com>
2019-09-02 20:31:28 +0530

    Completed passport Authentication

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
+// Passport Config
+require('./config/passport')(passport);
+
 // DB Config
-const db = require('./config/keys').MongoURI;
+const db = require('./config/keys').mongoURI;
 
-// Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true})
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//EJS
-app.use(expressLayouts);//using the middleware
+// EJS
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Bodyparser
-app.use(express.urlencoded({ extended: false }));
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
-//Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'))
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 5000;
+// Connect flash
+app.use(flash());
 
-app.listen(PORT,()=>{
-console.log(`Server started on port ${PORT}`);
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+
+dbPassword = 'mongodb+srv://Ramanan_gee:9952916905r@cluster10-wlvd7.mongodb.net/test?retryWrites=true&w=majority';
+
 module.exports = {
-    MongoURI: 'mongodb+srv://Ramanan_gee:9952916905r@cluster10-wlvd7.mongodb.net/test?retryWrites=true&w=majority'
-}
+    mongoURI: dbPassword
+};
